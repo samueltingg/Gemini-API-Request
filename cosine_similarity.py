ba777c7aac9f736af00613be52c758067e282b65
Samuel Ting <samuelting@Samuels-MacBook-Pro.local>
2025-07-26 14:55:30 +0800

    feat: converted data from csv all to 'vector embeddings' (in file: duplicate_detection_vector_embedding)

---
+++ cosine_similarity.py
@@ -0,0 +1,34 @@
+from dotenv import load_dotenv
+from google import genai
+import os
+from google.genai import types
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+
+# Load API key from .env
+load_dotenv()
+api_key = os.getenv("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
+texts = [
+    "What is the meaning of life?",
+    "What is the purpose of existence?",
+    "How do I bake a cake?"]
+
+result = [
+    np.array(e.values) for e in client.models.embed_content(
+        model="gemini-embedding-001",
+        contents=texts,
+        config=types.EmbedContentConfig(task_type="SEMANTIC_SIMILARITY")).embeddings
+]
+
+# Calculate cosine similarity. Higher scores = greater semantic similarity.
+
+embeddings_matrix = np.array(result)
+similarity_matrix = cosine_similarity(embeddings_matrix)
+
+for i, text1 in enumerate(texts):
+    for j in range(i + 1, len(texts)):
+        text2 = texts[j]
+        similarity = similarity_matrix[i, j]
+        print(f"Similarity between '{text1}' and '{text2}': {similarity:.4f}")
